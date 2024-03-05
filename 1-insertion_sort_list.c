54198bbd5b5ef32e8ba3fe4a0003556c5aa8057d
SeM2x <houssem135.302@gmail.com>
2024-03-05 12:17:54 +0100

    1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,24 +7,26 @@
  * @node2: node 2
  * Return: a pointer to the swapped node.
  */
-listint_t *swap_nodes(listint_t *node1, listint_t *node2)
+listint_t *swap_nodes(listint_t **list, listint_t *node1, listint_t *node2)
 {
-    listint_t *next, *prev;
+	listint_t *next, *prev;
 
-    prev = node2->prev;
-    next = node1->next;
+	prev = node2->prev;
+	next = node1->next;
 
-    node2->next = next;
-    node1->next = node2;
-    if (prev)
-        prev->next = node1;
+	node2->next = next;
+	node1->next = node2;
+	if (prev)
+		prev->next = node1;
+	else
+		*list = node1;
 
-    node2->prev = node1;
-    node1->prev = prev;
-    if (next)
-        next->prev = node2;
+	node2->prev = node1;
+	node1->prev = prev;
+	if (next)
+		next->prev = node2;
 
-    return (node2);
+	return (node2);
 }
 /**
  * insertion_sort_list - sorts a doubly linked list of integers
@@ -34,31 +36,26 @@ listint_t *swap_nodes(listint_t *node1, listint_t *node2)
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *node = NULL, *current = NULL;
+	listint_t *node = NULL, *current = NULL;
 
-    if (!(*list))
-        return;
+	if (!(*list))
+		return;
 
-    node = (*list)->next;
-    while (node)
-    {
-        if (node->n < node->prev->n)
-        {
-            node = swap_nodes(node, node->prev);
-            print_list(*list);
-            current = node->prev;
-            while (current && current->prev && current->n < current->prev->n)
-            {
-                current = swap_nodes(current, current->prev);
-                if (!current->prev->prev)
-                    print_list(current->prev);
-                else
-                    print_list(*list);
-                current = current->prev;
-            }
-            if (!current->prev)
-                *list = current;
-        }
-        node = node->next;
-    }
+	node = (*list)->next;
+	while (node)
+	{
+		if (node->n < node->prev->n)
+		{
+			node = swap_nodes(list, node, node->prev);
+			print_list(*list);
+			current = node->prev;
+			while (current && current->prev && current->n < current->prev->n)
+			{
+				current = swap_nodes(list, current, current->prev);
+				print_list(*list);
+				current = current->prev;
+			}
+		}
+		node = node->next;
+	}
 }
